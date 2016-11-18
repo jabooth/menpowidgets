0e02d49b93ee7ccef8711357a840cb3a321ce331
steliosploumpis <steliosploumpis@gmail.com>
2016-11-18 11:49:07 +0000

    First attempt viewing a coloured trimesh in NICP

--- menpowidgets/menpo3dwidgets/nicp.py
+++ menpowidgets/menpo3dwidgets/nicp.py
@@ -2,36 +2,63 @@ from collections import Sized
 import IPython.display as ipydisplay
 import ipywidgets
 from mayavi import mlab
-
+import numpy as np
 from menpowidgets.style import map_styles_to_hex_colours
 from menpowidgets.options import (AnimationOptionsWidget, TextPrintWidget,
                                   SaveMayaviFigureOptionsWidget)
 from menpowidgets.tools import LogoWidget
+from menpo.shape import PointCloud, ColouredTriMesh, TriMesh
 
 
-def view_landmark_displacement(source, target, group=None):
+def visualize_used_points(template, w_i_n, colour=(0.2, 0.8, 0.3)):
+    colours = np.ones_like(template.points)
+    colours[w_i_n, 0] = colour[0]
+    colours[w_i_n, 1] = colour[1]
+    colours[w_i_n, 2] = colour[2]
+    return ColouredTriMesh(template.points, trilist=template.trilist,
+                           colours=colours)
+
+
+def view_landmark_displacement(source, target, renderer, group=None):
+    from menpo3d.visualize.viewmayavi import MayaviVectorViewer3d
     source_lms = source.landmarks[group].lms
     target_lms = target.landmarks[group].lms
 
-    source_lms.view(new_figure=False, marker_colour=(1, 0, 0.5),
+    source_lms.view(new_figure=False, marker_colour=(1, 1, 0),
                     marker_size=0.03)
-    target_lms.view(new_figure=False, marker_colour=(1, 1, 0),
+    target_lms.view(new_figure=False, marker_colour=(1, 0, 0.5),
                     marker_size=0.03)
 
     points = source_lms.points
     diff = target_lms.points - source_lms.points
-    mlab.quiver3d(points[:, 0], points[:, 1], points[:, 2],
-                  diff[:, 0], diff[:, 1], diff[:, 2])
+    MayaviVectorViewer3d(figure_id=renderer.figure, new_figure=False,
+                         points=points, vectors=diff)
+    # mlab.quiver3d(points[:, 0], points[:, 1], points[:, 2],
+    #               diff[:, 0], diff[:, 1], diff[:, 2])
 
 
 def deformation_visualization(current_instance, initial_instance, aligned_mesh,
-                              group=None):
+                              renderer, group=None, w_i_n=None):
     in_landmarks = current_instance.landmarks[group].lms
-    figure = in_landmarks.view(new_figure=False, marker_size=0.03,
-                               marker_colour=(0, 0.3, 1))
-    current_instance.view(new_figure=False, colour=(0, 1, 0.5))
-    view_landmark_displacement(initial_instance, aligned_mesh, group=group)
+    #figure = in_landmarks.view(new_figure=False, marker_size=0.03,
+    # marker_colour=(0, 0.3, 1))
+    if w_i_n is not None:
+        colour = (0.2, 0.8, 0.3)
+        #coloured_mesh = visualize_used_points(current_instance, w_i_n)
+        #coloured_mesh.view(new_figure=False)
+        colours = np.ones_like(current_instance.points)
+        colours[w_i_n, 0] = colour[0]
+        colours[w_i_n, 1] = colour[1]
+        colours[w_i_n, 2] = colour[2]
+        current_instance = ColouredTriMesh(current_instance.points,
+                                           trilist=current_instance.trilist,
+                                           colours=colours)
+    r = current_instance.view(new_figure=False, figure_id=renderer.figure)
+    view_landmark_displacement(initial_instance, aligned_mesh,
+                               renderer, group=group)
 
+    # return the colouredtrimesh actor (we need to clear it!)
+    return r._actors[0]
 
 def visualize_nicp(nicp_results, source, target, group=None, style='coloured',
                    browser_style='buttons'):
@@ -67,17 +94,19 @@ def visualize_nicp(nicp_results, source, target, group=None, style='coloured',
 
     # Define render function
     def render_function(_):
-
         # Clear figure
         save_figure_wid.renderer.clear_figure()
 
+        if hasattr(save_figure_wid, '__actor'):
+            # manually clear the colourtrimesh for now!
+            save_figure_wid.renderer.figure.scene.remove_actors(save_figure_wid.__actor)
+            del save_figure_wid.__actor
         # Render instance
         id_ = result_number_wid.selected_values if n_results > 1 else 0
-        deformation_visualization(nicp_results[id_][0], source, target,
-                                  group=group)
-
-        mlab.draw()
-
+        actor = deformation_visualization(nicp_results[id_][0], source, target,
+                                  save_figure_wid.renderer, group=group,
+                                  w_i_n=nicp_results[id_][2])
+        save_figure_wid.__actor = actor
         update_info(nicp_results[id_][1])
 
         save_figure_wid.renderer.force_draw()
@@ -85,10 +114,10 @@ def visualize_nicp(nicp_results, source, target, group=None, style='coloured',
     # Define function that updates the info text
     def update_info(info):
         text_per_line = [
-            "> Alpha {}".format(info['alpha']),
-            "> Alpha {}".format(info['beta']),
+            "> Stiffness Weight: {}".format(info['alpha']),
+            "> Landmark Weight: {}".format(info['beta']),
             "> Omitted {:.2%}".format(info['prop_omitted']),
-            "> Alpha {}".format(info['alpha'])
+            "> Normal Error: {:.2%}".format(info['prop_omitted_norms'])
         ]
         info_wid.set_widget_state(text_per_line=text_per_line)
 
